e4644602fd4a78d57a73ec7f43d6b8b51780633b
Faraz Khan <faraz.khan14158124@gmail.com>
2023-03-23 13:08:36 +0530

    Update App
    using useContext api

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React from "react";
 import Counter from "./Counter";
 import CounterHooks from "./CounterHooks";
+import { useState } from "react";
+
+export const ThemeContext = React.createContext();
 
 function App() {
+  const [theme, setTheme] = useState("red");
   return (
-    <>
+    <ThemeContext.Provider value={{ backgroundColor: theme }}>
       <h1>Class Counter</h1>
       <Counter initialCount={10} />
       <h1>Function Counter</h1>
       <CounterHooks initialCount={10} />
-    </>
+      <button
+        onClick={() =>
+          setTheme((prevTheme) => (prevTheme === "red" ? "blue" : "red"))
+        }
+      >
+        Toggle Theme
+      </button>
+    </ThemeContext.Provider>
   );
 }
 
